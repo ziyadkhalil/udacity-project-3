78ec1be654c15f563e7409ea4f46070e71ed4714
Ziyad Khalil <ziyadhkhalil@gmail.com>
2022-08-07 21:44:28 +0200

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [backendResponse, setBackendResponse] = useState();
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}`)
+      .then((res) => setBackendResponse(res.data));
+  }, []);
+  return (
+    <div>
+      <h3>Backend response:</h3>
+      {backendResponse}
+    </div>
+  );
+}
+
+export default App;
